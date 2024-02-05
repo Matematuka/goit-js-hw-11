f814e694d363ca8cff5479e9658ef1ece36f2007
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-02-05 12:33:37 +0200

    Edits

--- src/main.js
+++ src/main.js
@@ -12,39 +12,41 @@ function searchImage(evt) {
     const image = evt.target.elements.image.value.trim();
     if (image === '') {
         iziToast.show({
-    title: 'Error',
-    message: 'Please enter a search term to begin your search.',   
-    titleSize: '16px',
-    titleLineHeight: '150%',
-    messageSize: '16px',
-    messageLineHeight: '150%',
-    backgroundColor: '#ef4040',
-    position: 'bottomRight'
-         }); 
-        return; 
+            title: 'Error',
+            message: 'Please enter a search term to begin your search.',
+            titleSize: '16px',
+            titleLineHeight: '150%',
+            messageSize: '16px',
+            messageLineHeight: '150%',
+            backgroundColor: '#ef4040',
+            position: 'bottomRight'
+        });
+        return;
     } else {
         pictures.innerHTML = '<span class="loader"></span>';
         getImage(image).then(data => {
             if (data.totalHits > 0) {
-    const markup = data.hits.map(imageTemplate).join('\n\n');
+                const markup = data.hits.map(imageTemplate).join('\n\n');
                 pictures.innerHTML = markup;
-                gallery.refresh();           
-            } else {
-        pictures.innerHTML = "";
-        iziToast.show({
-    title: 'Error',
-    message: 'There are no images matching your search query. Please try again!',   
-    titleSize: '16px',
-    titleLineHeight: '150%',
-    messageSize: '16px',
-    messageLineHeight: '150%',
-    backgroundColor: '#ef4040',
-    position: 'bottomRight'
-        });              
-        }       
-        });     
-}
-    evt.target.reset();   
+                gallery.refresh();
+            }
+        }
+         ).catch((error) => {
+                pictures.innerHTML = "";
+                iziToast.show({
+                    title: 'Error',
+                    message: 'There are no images matching your search query. Please try again!',
+                    titleSize: '16px',
+                    titleLineHeight: '150%',
+                    messageSize: '16px',
+                    messageLineHeight: '150%',
+                    backgroundColor: '#ef4040',
+                    position: 'bottomRight'
+                });
+            }
+        );
+    }
+    evt.target.reset();
 }
 
 
@@ -52,18 +54,18 @@ function getImage(imageName) {
     const BASE_URL = 'https://pixabay.com/api';
     const PARAMS = `?key=42174217-6daf07c41ac875e98ae2151fa&q=${imageName}&image_type=photo$orientation=horizontal&safesearch=true`;
     const url = BASE_URL + PARAMS;
-    return fetch(url).then(res => res.json());
+    return fetch(url).then(res => res.json()).catch();
 }
 
-function imageTemplate ({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) {
+function imageTemplate({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
     return `<li class="gallery-item"><a href="${largeImageURL}"><img class="gallery-image" src="${webformatURL}" alt="${tags}" /></a>
 <div class="description"> <p>Likes <span>${likes}</span></p><p>Views <span>${views}</span></p><p>Comments <span>${comments}</span></p><p>Downloads <span>${downloads}</span></p></div></li>`;
 }
 
 const gallery = new SimpleLightbox('.gallery a', {
-   captionsData: 'alt',
-   captionDelay: 250,
- });
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 
 
